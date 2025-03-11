97cdbd4de1d3db95089d5fdebf347e344c825496
gcd418 <patosquare418@gmail.com>
2025-03-10 22:49:31 -0400

    style: modificación de orden de salida

--- src/presenter.js
+++ src/presenter.js
@@ -29,15 +29,15 @@ form.addEventListener("submit", (event) => {
   div.innerHTML = `
     <p>Cantidad: ${totalizador.cantidadDeItems}</p>
     <p>Precio: ${totalizador.precioDeItems}</p>
+    <p>Precio Neto (${totalizador.cantidadDeItems} * \$${totalizador.precioDeItems}): ${totalizador.precioNeto}</p>
     <p>Descuento: ${totalizador.porcentajeDescuento}%</p>
     <p>Código de Estado seleccionado: ${totalizador.codigoDeEstado}</p>
+    <p>Impuesto para ${totalizador.codigoDeEstado} = (%${totalizador.impuesto})</p>
     <p>Categoría de producto seleccionada: ${categoriaDeProducto}</p>
     <p>Impuesto de la categoría de producto: ${totalizador.impuestoCategoriaProducto}%</p>
     <p>Descuento de la categoría de producto: ${totalizador.descuentoCategoriaProducto}%</p>
     <p>Descuento del tipo de usuario: ${totalizador.descuentoPorUsuario}%</p>
-    <p>Impuesto para ${totalizador.codigoDeEstado} = (%${totalizador.impuesto})</p>
     <p>Peso Volumétrico: ${_pesoVolumetrico.value} kg</p>
-    <p>Precio Neto (${totalizador.cantidadDeItems} * \$${totalizador.precioDeItems}): ${totalizador.precioNeto}</p>
     <p>Precio Total(Con impuesto únicamente): \$${totalizador.calcularPrecioTotal().toFixed(2)}</p>
     <p>Costo por Peso Volumétrico: \$${totalizador.costoPesoVolumetrico.toFixed(2)}</p> 
 `;
